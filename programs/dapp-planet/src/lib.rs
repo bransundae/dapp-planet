044555634a85e8952fba8b6ec642ac3f0315f80d
bransundae <bjohnso@student.wethinkcode.co.za>
2023-04-24 23:22:02 +0200

    front end scaffold

--- programs/dapp-planet/src/lib.rs
+++ programs/dapp-planet/src/lib.rs
@@ -6,10 +6,24 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod dapp_planet {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn deploy_testing_closed(ctx: Context<DeployTestingClosed>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn deploy_testing_open(ctx: Context<DeployTestingOpen>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn deploy_production(ctx: Context<DeployProduction>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct DeployTestingClosed {}
+
+#[derive(Accounts)]
+pub struct DeployTestingOpen {}
+
+#[derive(Accounts)]
+pub struct DeployProduction {}
